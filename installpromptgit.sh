6eb16278fe3bc50d7bdaef41274d65eab07bf943
TristeroEmpire <diogene.le.cynique@gmail.com>
2015-09-13 13:18:02 +0200

    Documentation, corrections diverses
    - Ajout d'une documentation pour plus de clarté
    - Suppression de variables inutiles (2)
    - Corrections de coquilles

--- installpromptgit.sh
+++ installpromptgit.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+
 #variables
 dl_directory=$(echo ~/)
 file_sh_git='git-prompt.sh'
-remote_git_file='git-prompt.sh'
 local_bashrc_sh_file='test.sh'
 bslash=$(echo $'\x5C'$'\x5C'$'\x5C')
+
+#Variables modifiant le bashrc
 text_to_add="#start adding modifs prompt-git.sh
 source ~/git-prompt.sh
 export GIT_PS1_SHOWDIRTYSTATE=1 GIT_PS1_SHOWSTASHSTATE=1 GIT_PS1_SHOWUNTRACKEDFILES=1
@@ -12,9 +14,10 @@ export GIT_PS1_SHOWUPSTREAM=verbose GIT_PS1_DESCRIBE_STYLE=branch
 #Enable color for git states
 export GIT_PS1_SHOWCOLORHINTS=1"
 prompt_var="export PROMPT_COMMAND='__git_ps1 \"\\u:\\w\" \" ${bslash}$ \"'"
-text_to_add_bslash=$text_to_add$bslash
 dled_file=$dl_directory$file_sh_git
-#functions
+
+#fonctions
+#Vérifie si un fichier passé en argument existe bel et bien
 check_file_exist () {
 if [ -f "$1" ]
   then
@@ -23,13 +26,20 @@ if [ -f "$1" ]
     return 0
   fi
 }
+
+#Télécharge via curl le script git-prompt depuis le dépôt officiel sur GitHub 
+#avec écriture dans le home de l'utilisateur
 downld_latest () {
-  curl https://raw.githubusercontent.com/git/git/dd160d794f0bf02c30d2e5032e216b1e8ac14222/contrib/completion/git-prompt.sh > $dl_directory$remote_git_file
+  curl https://raw.githubusercontent.com/git/git/dd160d794f0bf02c30d2e5032e216b1e8ac14222/contrib/completion/git-prompt.sh > $dl_directory$file_sh_git
 }
+
+#Ajoute en fin de fichier .bashrc les éléments en arguments
 add_to_eof () {
   echo -e "$1" >> $dl_directory$local_bashrc_sh_file
   echo -E "$2" >> $dl_directory$local_bashrc_sh_file
 }
+
+#Point d'entrée du script
 main () {
 check_file_exist $dled_file
 if [ $? == 0 ]
@@ -37,16 +47,16 @@ if [ $? == 0 ]
   echo "Downloading latest version from github..."
   downld_latest
   else
-    echo -e "File already exist\nDo you want to overwrite/update it, yes(y) or no(N) ?"
+    echo -e "File already exist\nDo you want to overwrite/update it, yes(y) or no(any keys) ?"
     read response
     if [ $response == y ] || [ $response == Y ]
       then
         echo "Downloading and overwirting file ..."
         downld_latest
       else
-        echo "Do you want to continue install, yes(y) or no(N) ?"
-        read choise
-        if [ $choise == y ] || [ $choise == yes ]
+        echo "Do you want to continue install, yes(y) or no(any keys) ?"
+        read choice
+        if [ $choice == y ] || [ $choice == yes ]
           then
             add_to_eof "$text_to_add" "$prompt_var"
           else
